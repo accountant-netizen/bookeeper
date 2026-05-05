2f3fc55716bc37afe9f90c2132f67163ad1ab64a
accountant-netizen <dennis.ezekielvidar@gmail.com>
2026-05-05 13:19:26 +0800

    wipe

--- supabase/migrations/202604260001_add_account_type_to_accounts.sql
+++
@@ -1,8 +0,0 @@
--- Add account type support for chart of accounts creation.
-
-alter table public.accounts
-  add column if not exists account_type text not null default 'general';
-
-update public.accounts
-set account_type = 'general'
-where account_type is null;
